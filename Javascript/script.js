88fe2f0e7ebe7e8ffaca7481b3e69f8b5d3375d7
GusV208 <130870232+GusV208@users.noreply.github.com>
2023-05-21 23:46:40 -0300

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,19 @@
+//Sólo lo enlace con la página index.html para mostrar los resultados.
+
+var nombre="Gustavo";
+var apellido="Valdez";
+
+console.log(nombre + " " + apellido)
+
+
+var numero1=3
+var numero2=4
+var suma=numero1+numero2
+
+console.log("El resultado de la sumas es " + suma)
+
+
+var numero3=suma + 5
+
+console.log("El resultado de la suma es " + numero3)
+
